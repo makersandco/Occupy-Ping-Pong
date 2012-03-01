b7240d9ed51c7d10209af0b6f3200c52b2f3bdb0
Sven Kraeuter | 5v3n <sven.kraeuter@gmx.net>
2012-03-01 23:26:11 +0100

    initial server the wiflys negoatiate with

--- opp_server/opp_server.rb
+++ opp_server/opp_server.rb
@@ -1,7 +1,18 @@
 require 'sinatra'
+require 'json'
 
 class OppServer < Sinatra::Application
+  @@occupied=0
   get '/' do
-    '<h1>Occupy Ping Pong!</h1>'
+    "<h1>Occupy Ping Pong!</h1><br/><h2>{\"occupied\": #{@@occupied}}</h2>"
+  end
+  get '/status.json' do
+    "{\"occupied\": #{@@occupied}}"
+  end
+  put '/status.json' do
+    request.body.rewind  # in case someone already read it
+    data = JSON.parse request.body.read
+    @@occupied = data['occupied']
+    "{\"occupied\": #{@@occupied}}"
   end
 end
